5ddcf034d21bcfd103ef6bf6c13099a3aeeb91f3
Dan1lD <davidian654@gmail.com>
2022-05-10 16:45:41 +0300

    refactoring of smartrating

--- core/basic_models/actions/smartrating.py
+++ core/basic_models/actions/smartrating.py
@@ -6,33 +6,35 @@ from core.text_preprocessing.base import BaseTextPreprocessingResult
 from scenarios.user.user_model import User
 
 
-class CallRatingAction(StringAction):
-    def __init__(self, items: Dict[str, Any], id: Optional[str] = None):
-        items["command"] = "CALL_RATING"
-        super().__init__(items, id)
+CALL_RATING = "CALL_RATING"
+ASK_RATING = "ASK_RATING"
+
+
+class SmartRatingAction(StringAction):
+    kafka_key = None
+    DEFAULT_KAFKA_KEY = "main"
 
     def run(self, user: User, text_preprocessing_result: BaseTextPreprocessingResult,
             params: Optional[Dict[str, Union[str, float, int]]] = None) -> Optional[List[Command]]:
+        settings_kafka_key = user.settings["template_settings"].get("smartrating_kafka_key")
         self.request_data = {
             "topic_key": "toDP",
-            "kafka_key": "main",
+            "kafka_key": self.kafka_key or settings_kafka_key or self.DEFAULT_KAFKA_KEY,
             "kafka_replyTopic": user.settings["template_settings"]["consumer_topic"]
         }
 
         return super().run(user, text_preprocessing_result, params)
 
 
-class AskRatingAction(StringAction):
+class CallRatingAction(SmartRatingAction):
     def __init__(self, items: Dict[str, Any], id: Optional[str] = None):
-        items["command"] = "ASK_RATING"
+        self.kafka_key = items.get("kafka_key")
+        items["command"] = CALL_RATING
         super().__init__(items, id)
 
-    def run(self, user: User, text_preprocessing_result: BaseTextPreprocessingResult,
-            params: Optional[Dict[str, Union[str, float, int]]] = None) -> Optional[List[Command]]:
-        self.request_data = {
-            "topic_key": "toDP",
-            "kafka_key": "main",
-            "kafka_replyTopic": user.settings["template_settings"]["consumer_topic"]
-        }
 
-        return super().run(user, text_preprocessing_result, params)
+class AskRatingAction(SmartRatingAction):
+    def __init__(self, items: Dict[str, Any], id: Optional[str] = None):
+        self.kafka_key = items.get("kafka_key")
+        items["command"] = ASK_RATING
+        super().__init__(items, id)
