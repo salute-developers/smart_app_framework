4b389eea696a98616f02041c79f93630ddacfdbc
Dmitrii Proskurin <doproskurin@sberbank.ru>
2022-06-24 16:43:33 +0300

    reply to topic kafka_replyTopic if it exists in message headers

--- core/request/kafka_request.py
+++ core/request/kafka_request.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from core.request.base_request import BaseRequest
 
 
@@ -10,10 +12,7 @@ class KafkaRequest(BaseRequest):
         items = items or {}
         self.topic_key = items.get(self.TOPIC_KEY)
         self.kafka_key = items.get(self.KAFKA_KEY)
-
-    def update_empty_items(self, items):
-        self.topic_key = self.topic_key or items["topic_key"]
-        self.kafka_key = self.kafka_key or items["kafka_key"]
+        self.reply_to_topic_name: Optional[str] = None
 
     @property
     def group_key(self):
@@ -27,7 +26,20 @@ class KafkaRequest(BaseRequest):
         publishers = params["publishers"]
         publisher = publishers[self.kafka_key]
         source_mq_message = params["mq_message"]
-        publisher.send(data, source_mq_message.key(), self.topic_key, headers=self._get_new_headers(source_mq_message))
+        if self.reply_to_topic_name:
+            publisher.send_to_topic(
+                value=data,
+                key=source_mq_message.key(),
+                topic=self.reply_to_topic_name,
+                headers=self._get_new_headers(source_mq_message),
+            )
+        else:
+            publisher.send(
+                value=data,
+                key=source_mq_message.key(),
+                topic_key=self.topic_key,
+                headers=self._get_new_headers(source_mq_message),
+            )
 
     def __str__(self):
         return f"KafkaRequest: topic_key={self.topic_key} kafka_key={self.kafka_key}"
