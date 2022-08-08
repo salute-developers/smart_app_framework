f69e1b0dc3763b5ae8df8dbef220856a08b1a0b4
Artem Batalov <g@batalov.me>
2022-08-08 16:33:53 +0300

    send in kafka_request

--- core/request/kafka_request.py
+++ core/request/kafka_request.py
@@ -1,5 +1,7 @@
 from typing import Dict
 
+from confluent_kafka.cimpl import Message as KafkaMessage
+from core.mq.kafka.kafka_publisher import KafkaPublisher
 from core.request.base_request import BaseRequest
 from core.logging.logger_utils import log
 import core.logging.logger_constants as log_const
@@ -27,16 +29,12 @@ class KafkaRequest(BaseRequest):
     def group_key(self):
         return "{}_{}".format(self.kafka_key, self.topic_key) if (self.topic_key and self.kafka_key) else None
 
-    def _get_new_headers(self, source_mq_message):
+    def _get_new_headers(self, source_mq_message: KafkaMessage):
         headers = source_mq_message.headers() or []
         return headers
 
-    def run(self, data, params=None):
-        publishers = params["publishers"]
-        publisher = publishers[self.kafka_key]
-        source_mq_message = params["mq_message"]
+    def send(self, data, publisher: KafkaPublisher, source_mq_message):
         headers = self._get_new_headers(source_mq_message)
-
         if self.topic_key is not None:
             publisher.send(data, source_mq_message.key(), self.topic_key, headers=headers)
         elif self.topic is not None:
@@ -46,7 +44,12 @@ class KafkaRequest(BaseRequest):
                 "data": str(data),
                 log_const.KEY_NAME: log_const.EXCEPTION_VALUE
             }
-            log("KafkaTopicRequest: got no topic and no topic_key", params=log_params, level="ERROR")
+            log("KafkaRequest: got no topic and no topic_key", params=log_params, level="ERROR")
+
+    def run(self, data, params=None):
+        publishers = params["publishers"]
+        publisher = publishers[self.kafka_key]
+        self.send(data=data, publisher=publisher, source_mq_message=params["mq_message"])
 
     def __str__(self):
         if self.topic_key is not None:
