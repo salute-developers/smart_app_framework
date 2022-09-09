e76bf86758f65abab1d1892da2f75920623f1e1c
Makar Shevchenko <makarsevchenko@gmail.com>
2022-09-09 17:42:01 +0300

    DPNLPF-1782: fixed tests on handlers

--- tests/smart_kit_tests/handlers/test_handle_take_profile_data.py
+++ tests/smart_kit_tests/handlers/test_handle_take_profile_data.py
@@ -26,9 +26,9 @@ class HandleTakeProfileDataTest(unittest.TestCase):
         self.test_payload_1 = {"server_action": {}}
         self.test_payload_2 = {"server_action": {"action_id": 1, "parameters": 1}}
         self.test_user = MagicMock('user', message=MagicMock(message_name="some_name"), variables=MockVariables(),
-                                   behaviors=MagicMock(success=lambda x: "success",
-                                                       fail=lambda x: "fail",
-                                                       timeout=lambda x: "timeout"))
+                                   behaviors=MagicMock(success=lambda x: ["success"],
+                                                       fail=lambda x: ["fail"],
+                                                       timeout=lambda x: ["timeout"]))
 
     def test_handle_take_profile_data_init(self):
         obj = handler_take_profile_data.HandlerTakeProfileData(self.app_name)
@@ -38,12 +38,12 @@ class HandleTakeProfileDataTest(unittest.TestCase):
     def test_handle_take_profile_data_run_fail(self):
         obj = handler_take_profile_data.HandlerTakeProfileData(self.app_name)
         payload = {"status_code": {"code": 102}}
-        self.assertEqual(obj.run(payload, self.test_user), "fail")
+        self.assertEqual(obj.run(payload, self.test_user), ["fail"])
 
     def test_handle_take_profile_data_run_success(self):
         obj = handler_take_profile_data.HandlerTakeProfileData(self.app_name)
         payload = {"profile_data": {"geo": {"reverseGeocoding": {"country": "Российская Федерация"},
                                             "location": {"lat": 10.125, "lon": 10.0124}}},
                    "status_code": {"code": 1}}
-        self.assertEqual(obj.run(payload, self.test_user), "success")
+        self.assertEqual(obj.run(payload, self.test_user), ["success"])
         self.assertEqual(self.test_user.variables.get("smart_geo"), payload["profile_data"]["geo"])
