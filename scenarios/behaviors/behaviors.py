2e294ec0e735f3f4974215202cbc7320f3222756
sharifullinildus <sharifullin.ildus@list.ru>
2022-10-29 10:57:54 +0300

    Убраны дублирующиеся логи

--- scenarios/behaviors/behaviors.py
+++ scenarios/behaviors/behaviors.py
@@ -193,10 +193,6 @@ class Behaviors:
         return result
 
     def misstate(self, callback_id: str) -> List[Command]:
-        log(f"behavior.misstate started: got callback %({log_const.BEHAVIOR_CALLBACK_ID_VALUE})s.",
-            self._user,
-            params={log_const.KEY_NAME: log_const.BEHAVIOR_MISSTATE_VALUE,
-                    log_const.BEHAVIOR_CALLBACK_ID_VALUE: callback_id})
         callback = self._get_callback(callback_id)
         result = []
         if callback:
