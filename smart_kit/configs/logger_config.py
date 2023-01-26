8303d674fe42d5495d010d43b37d328479414ed6
Dmitrii Proskurin <doproskurin@sberbank.ru>
2023-01-26 17:58:49 +0300

    add version of NLPF to logs

--- smart_kit/configs/logger_config.py
+++ smart_kit/configs/logger_config.py
@@ -1,17 +1,35 @@
 # coding: utf-8
 import os
+from typing import Optional
 
+import pkg_resources
 import yaml
 
-import versioneer
 from core.configs.base_config import BaseConfig
 from core.repositories.file_repository import FileRepository
 from smart_kit.utils.logger_writer.logger_formatter import SmartKitJsonFormatter
 
-try:
-    SmartKitJsonFormatter.VERSION = os.getenv("VERSION") or versioneer.get_version()
-except:
-    SmartKitJsonFormatter.VERSION = 0
+
+def _get_distribution_safe(name: str) -> Optional[pkg_resources.DistInfoDistribution]:
+    try:
+        distribution = pkg_resources.get_distribution(name)
+        return distribution
+    except pkg_resources.DistributionNotFound:
+        return None
+
+
+def setup_version():
+    version = os.getenv("VERSION")
+    if version is None:
+        distribution = _get_distribution_safe("sber-nlp-platform-smart-app-framework")
+        if distribution is None:
+            distribution = _get_distribution_safe("smart-app-framework")
+        version = distribution.version if distribution is not None else 0
+
+    SmartKitJsonFormatter.VERSION = version
+
+
+setup_version()
 
 
 LOGGING_CONFIG = "logging_config"
