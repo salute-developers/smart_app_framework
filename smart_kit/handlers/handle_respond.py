f5f67410492c43beebf66ea3aabe3dcdfd047cf4
Makar Shevchenko <makarsevchenko@gmail.com>
2023-03-28 12:45:16 +0300

    DPNLPF-2013: remove dubbling of monitoring.counter_incoming in handlers

--- smart_kit/handlers/handle_respond.py
+++ smart_kit/handlers/handle_respond.py
@@ -10,7 +10,6 @@ from scenarios.user.user_model import User
 
 from smart_kit.handlers.handler_base import HandlerBase
 from smart_kit.names.action_params_names import TO_MESSAGE_NAME, SEND_TIMESTAMP
-from core.monitoring.monitoring import monitoring
 
 
 class HandlerRespond(HandlerBase):
@@ -46,8 +45,6 @@ class HandlerRespond(HandlerBase):
             log("HandlerRespond with action %(action_name)s started respond on %(to_message_name)s", user, params)
         else:
             log("HandlerRespond with action %(action_name)s started without callback", user, params)
-
-        monitoring.counter_incoming(self.app_name, user.message.message_name, self.__class__.__name__, user)
 
         text_preprocessing_result = TextPreprocessingResult.from_payload(payload)
 
