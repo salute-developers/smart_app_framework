2a9ce04e302777e204e35de0b056e91d8e4aae41
Artem Batalov <g@batalov.me>
2023-04-13 13:57:16 +0300

    Revert "cast log_params['params'] to str after checking for log-level (#54)"
    This reverts commit ea021b83668660093cabe71d4bb81f7fe12c14f4.

--- core/logging/logger_utils.py
+++ core/logging/logger_utils.py
@@ -96,11 +96,6 @@ def log(message, user=None, params=None, level="INFO", exc_info=None, log_store_
         except AttributeError:
             message_maker = LoggerMessageCreator
 
-        # cast log_params["params"] to str
-        # TODO: think how to make it more general
-        if params and "params" in params:
-            params["params"] = str(params["params"])
-
         log_store_for_map = getattr(logging, "log_store_for_map", None)
         if log_store_for_map is not None and params is not None:
             log_store_for = log_store_for_map.get(params.get(log_const.KEY_NAME), log_store_for)
