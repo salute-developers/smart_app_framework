05f72eca991008363857224da610c403f00150ac
Dmitrii Proskurin <doproskurin@sberbank.ru>
2023-10-04 00:19:32 +0300

    upd exc_handler

--- core/utils/exception_handlers.py
+++ core/utils/exception_handlers.py
@@ -8,45 +8,21 @@ def exc_handler(on_error_obj_method_name=None, handled_exceptions=None):
     handled_exceptions = tuple(handled_exceptions) if handled_exceptions else (Exception,)
 
     def exc_handler_decorator(funct):
-        if asyncio.iscoroutinefunction(funct):
+        if inspect.isasyncgenfunction(funct):
             @wraps(funct)
             async def _wrapper(obj, *args, **kwarg):
-                result = None
                 try:
-                    result = await funct(obj, *args, **kwarg)
+                    async for it in funct(obj, *args, **kwarg):
+                        yield it
                 except handled_exceptions:
                     try:
                         on_error = (
                             getattr(obj, on_error_obj_method_name)
                             if on_error_obj_method_name else (lambda *x, **y: None)
                         )
-                        result = (
-                            on_error(*args, **kwarg)
-                            if not asyncio.iscoroutinefunction(on_error)
-                            else await on_error(*args, **kwarg)
-                        )
-                    except Exception:
-                        print(sys.exc_info())
-                return result
-
-            return _wrapper
-        elif inspect.isasyncgenfunction(funct):
-            @wraps(funct)
-            async def _wrapper(obj, *args, **kwarg):
-                try:
-                    async for yield_ in funct(obj, *args, **kwarg):
-                        yield yield_
-                except handled_exceptions:
-                    try:
-                        on_error = (
-                            getattr(obj, on_error_obj_method_name)
-                            if on_error_obj_method_name else (lambda *x, **y: None)
-                        )
-                        if asyncio.iscoroutinefunction(on_error):
-                            yield await on_error(*args, **kwarg)
-                        elif inspect.isasyncgenfunction(on_error):
-                            async for yield_ in on_error(*args, **kwarg):
-                                yield yield_
+                        if inspect.isasyncgenfunction(on_error):
+                            async for it in on_error(*args, **kwarg):
+                                yield it
                         else:
                             yield on_error(*args, **kwarg)
                     except Exception:
