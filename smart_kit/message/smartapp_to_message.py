5a0b58b2b76a098d739345005efd8ab2fff713e2
Danil Davydyan <45461378+Dan1lD@users.noreply.github.com>
2023-10-05 13:25:27 +0300

    add argument white_list to masking function (#146)
    * add argument white_list to masking function
    
    * add masking_white_list to SmartAppToMessage
    
    * fix annotations for SmartAppToMessage

--- smart_kit/message/smartapp_to_message.py
+++ smart_kit/message/smartapp_to_message.py
@@ -1,20 +1,26 @@
+from __future__ import annotations
+
 from functools import cached_property
-from typing import Iterable
+from typing import TYPE_CHECKING, Iterable, Optional, List
 import json
 from copy import copy
 
 from core.utils.masking_message import masking
-from core.message.msg_validator import MessageValidator
-from smart_kit.request.kafka_request import SmartKitKafkaRequest
 from smart_kit.utils import SmartAppToMessage_pb2
+
+if TYPE_CHECKING:
+    from core.basic_models.actions.command import Command
+    from core.message.msg_validator import MessageValidator
+    from smart_kit.request.kafka_request import SmartKitKafkaRequest
 
 
 class SmartAppToMessage:
     ROOT_NODES_KEY = "root_nodes"
     PAYLOAD = "payload"
 
-    def __init__(self, command, message, request: SmartKitKafkaRequest, forward_fields=None, masking_fields=None,
-                 validators: Iterable[MessageValidator] = (), **kwargs):
+    def __init__(self, command: Command, message, request: SmartKitKafkaRequest,
+                 forward_fields=None, masking_fields=None, validators: Iterable[MessageValidator] = (),
+                 masking_white_list: Optional[List[str]] = None, **kwargs):
         root_nodes = command.payload.pop(self.ROOT_NODES_KEY, None)
         self.command = command
         self.root_nodes = root_nodes or {}
@@ -22,6 +28,7 @@ class SmartAppToMessage:
         self.request = request
         self.forward_fields = forward_fields or ()
         self.masking_fields = masking_fields
+        self.masking_white_list = masking_white_list
         self.validators = validators
         self._kwargs = kwargs
 
