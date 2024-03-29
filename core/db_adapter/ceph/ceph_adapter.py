9b860063dceb4bccc6e3e1ab1c7607dd2b5f26a3
Andrey Pushnin <pushnin.a.an@sberbank.ru>
2024-03-29 12:38:43 +0300

    reverted _create_unverified_context

--- core/db_adapter/ceph/ceph_adapter.py
+++ core/db_adapter/ceph/ceph_adapter.py
@@ -11,6 +11,7 @@ from core.db_adapter.db_adapter import DBAdapter
 from core.logging.logger_utils import log
 from core.monitoring.monitoring import monitoring
 
+ssl._create_default_https_context = ssl._create_unverified_context
 
 class CephAdapter(DBAdapter):
     def __init__(self, config):
