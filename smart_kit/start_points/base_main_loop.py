1a272c5e3bf56b583c536cf1d2b1c17e586ec9b0
Kirill Krivov <kir.krivov@gmail.com>
2025-04-30 09:27:18 +0300

    Prevent db_uid from consisting of None

--- smart_kit/start_points/base_main_loop.py
+++ smart_kit/start_points/base_main_loop.py
@@ -1,6 +1,4 @@
-# coding=utf-8
-
-from typing import Type, Iterable
+from typing import Type, Iterable, Optional
 import asyncio
 import signal
 
@@ -12,9 +10,9 @@ from core.message.from_message import SmartAppFromMessage
 from core.monitoring.monitoring import monitoring
 from core.monitoring.healthcheck_handler import RootResource
 from core.monitoring.twisted_server import TwistedServer
-from core.model.base_user import BaseUser
 from core.basic_models.parametrizers.parametrizer import BasicParametrizer
 from core.message.msg_validator import MessageValidator
+from scenarios.user.user_model import User
 from smart_kit.start_points.postprocess import PostprocessMainLoop
 from smart_kit.models.smartapp_model import SmartAppModel
 
@@ -23,7 +21,7 @@ class BaseMainLoop:
 
     def __init__(
             self, model: SmartAppModel,
-            user_cls: Type[BaseUser],
+            user_cls: Type[User],
             parametrizer_cls: Type[BasicParametrizer],
             postprocessor_cls: Type[PostprocessMainLoop],
             settings,
@@ -98,20 +96,26 @@ class BaseMainLoop:
         monitoring.apply_config(monitoring_config)
         monitoring.init_metrics(app_name=self.app_name)
 
-    async def load_user(self, db_uid, message: SmartAppFromMessage):
-        db_data = None
-        load_error = False
+    async def load_user(self, db_uid: Optional[str], message: SmartAppFromMessage) -> User:
+        if db_uid is None:
+            log("Failed to load user data as db_uid is None. Will use empty user.", level="ERROR")
+            return self.get_user(message, db_data=None, load_error=True)
+        return await self._load_user(db_uid, message)
+
+    async def _load_user(self, db_uid: str, message: SmartAppFromMessage) -> User:
         try:
             db_data = await self.db_adapter.get(db_uid)
         except (DBAdapterException, ValueError):
             log("Failed to get user data", params={log_const.KEY_NAME: log_const.FAILED_DB_INTERACTION,
                                                    log_const.REQUEST_VALUE: message.as_str}, level="ERROR")
-            load_error = True
             monitoring.counter_load_error(self.app_name)
             # to skip message when load failed
             raise
+        return self.get_user(message, db_data, load_error=False)
+
+    def get_user(self, message: SmartAppFromMessage, db_data: Optional[dict], load_error: bool) -> User:
         return self.user_cls(
-            message.uid,
+            id=message.uid,
             message=message,
             db_data=db_data,
             settings=self.settings,
@@ -120,27 +124,37 @@ class BaseMainLoop:
             load_error=load_error
         )
 
-    async def save_user(self, db_uid, user, message: SmartAppFromMessage):
-        no_collisions = True
+    async def save_user(self, db_uid: Optional[str], user: User, message: SmartAppFromMessage) -> bool:
+        """
+        :return: True if there were no any collisions when saving, False otherwise
+        """
+        if db_uid is None:
+            log("User %(uid)s will not be saved as db_uid is None",
+                user=user, level="ERROR", params={"uid": user.id})
+            return True
+        return await self._save_user(db_uid, user, message)
+
+    async def _save_user(self, db_uid: str, user: User, message: SmartAppFromMessage) -> bool:
         if user.do_not_save:
-            log("User %(uid)s will not saved", user=user, params={"uid": user.id,
-                                                                  log_const.KEY_NAME: "user_will_not_saved"})
-        else:
-            no_collisions = True
-            try:
-                str_data = user.raw_str
-                if user.initial_db_data and self.user_save_check_for_collisions:
-                    no_collisions = await self.db_adapter.replace_if_equals(db_uid,
-                                                                            sample=user.initial_db_data,
-                                                                            data=str_data)
-                else:
-                    await self.db_adapter.save(db_uid, str_data)
-            except (DBAdapterException, ValueError):
-                log("Failed to set user data", params={log_const.KEY_NAME: log_const.FAILED_DB_INTERACTION,
-                                                       log_const.REQUEST_VALUE: message.as_str}, level="ERROR")
-                monitoring.counter_save_error(self.app_name)
-            if not no_collisions:
-                monitoring.counter_save_collision(self.app_name)
+            log("User %(uid)s will not be saved", user=user,
+                params={"uid": user.id, log_const.KEY_NAME: "user_will_not_saved"})
+            return True
+
+        no_collisions = True
+        try:
+            str_data = user.raw_str
+            if user.initial_db_data and self.user_save_check_for_collisions:
+                no_collisions = await self.db_adapter.replace_if_equals(db_uid,
+                                                                        sample=user.initial_db_data,
+                                                                        data=str_data)
+            else:
+                await self.db_adapter.save(db_uid, str_data)
+        except (DBAdapterException, ValueError):
+            log("Failed to set user data", params={log_const.KEY_NAME: log_const.FAILED_DB_INTERACTION,
+                                                   log_const.REQUEST_VALUE: message.as_str}, level="ERROR")
+            monitoring.counter_save_error(self.app_name)
+        if not no_collisions:
+            monitoring.counter_save_collision(self.app_name)
         return no_collisions
 
     def run(self):
